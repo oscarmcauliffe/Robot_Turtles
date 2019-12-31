e80acfac2cfc12b0999b42c9748bb37cc596d2a8
oscarmcauliffe <oscarmcauliffe99@gmail.com>
2019-12-31 17:37:34 +0100

    try catch in Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,26 @@
+import javax.net.ssl.SSLContext;
 import java.util.ArrayDeque;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Nombre de joueurs?\n");
-        Scanner scanner = new Scanner(System.in);
-        int nbJoueur = scanner.nextInt();
-
-        Jeu.nouvellePartie(nbJoueur);
+        boolean error = false;
+        while (!error){
+            try{
+                System.out.print("Nombre de joueurs?\n");
+                Scanner scanner = new Scanner(System.in);
+                int nbJoueur = scanner.nextInt();
+                if (nbJoueur == 2 || nbJoueur == 3 || nbJoueur == 4){
+                    error = true;
+                    Jeu.nouvellePartie(nbJoueur);
+                }
+                else {
+                    throw new Exception("Error");
+                }
+            }catch (Exception e){
+                System.out.println("2, 3 ou 4 joueurs svp.");
+            }
+        }
     }
 
 
