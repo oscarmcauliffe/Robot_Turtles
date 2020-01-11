278ef905cbe38e79142ab3a9d548b5f359e2b211
oscarmcauliffe <oscarmcauliffe99@gmail.com>
2020-01-11 04:04:59 +0100

    Réglages Obstacles

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -1,41 +1,33 @@
 import java.util.ArrayList;
 
 public class Obstacle extends Tuile{
-    String type;
 
-    public Obstacle(String t){
-        switch (t){
-            case "pierre":{
-                type = t;
-                break;
-            }
-            case "glace":{
-                type = t;
-                break;
-            }
+    public Obstacle(String n){
+        switch (n){
+            case "pierre":
+            case "glace":
             case "caisse":{
-                type = t;
+                nom = n;
                 break;
             }
             default:{
-                type = "pierre";
+                nom = "pierre";
                 break;
             }
         }
-        nom = type;
     }
 
     public static ArrayList<Obstacle> listeObstacles(){
-        ArrayList<Obstacle> oJ = new ArrayList();
+        ArrayList<Obstacle> pioche = new ArrayList();
         for (int i = 0; i < 3; i++){
-            oJ.add(new Obstacle("pierre"));
+            pioche.add(new Obstacle("pierre"));
         }
         for (int i = 0; i < 2; i++){
-            oJ.add(new Obstacle("glace"));
+            pioche.add(new Obstacle("glace"));
         }
         for (int i = 0; i < 1; i++){
-            oJ.add(new Obstacle("caisse"));
+            pioche.add(new Obstacle("caisse"));
         }
-        return oJ;
+        return pioche;
     }
 }
